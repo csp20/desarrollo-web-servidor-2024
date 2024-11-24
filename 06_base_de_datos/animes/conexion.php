7ae54d10509bcce047d137b62aaa9a07f64360dc
csp0017 <barrabinoguachupino@gmail.com>
2024-11-24 23:31:17 +0100

    subo act de bbdd casa 24 nov

--- 06_base_de_datos/animes/conexion.php
+++ 06_base_de_datos/animes/conexion.php
@@ -1,11 +1,16 @@
 <?php
-    $_servidor = "127.0.0.1"; // "localhost"
-    $_usuario = "estudiante";
-    $_contraseña = "estudiante";
+    $_servidor = "127.0.0.1"; // O "localhost"
+    $_usuario = "csp0017";
+    $_contrasena = "Guacamayo32000";
     $_base_de_datos = "animes_bd";
 
-    // Mysqli o PDO
+    // Crear conexión
+    $_conexion = new mysqli($_servidor, $_usuario, $_contrasena, $_base_de_datos);
 
-    $_conexion = new Mysqli($_servidor,$_usuario,$_contraseña,$_base_de_datos)
-        or die("Error de conexion");
+    // Verificar la conexión
+    if ($_conexion->connect_error) {
+        die("Error de conexión: " . $_conexion->connect_error);
+    }
+
+    echo "Conexión exitosa a la base de datos";
 ?>
